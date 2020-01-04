0b86ea00e00acfa712aa8877b8b11826d62039f2
Moritz <m.p.schick@web.de>
2020-01-04 18:12:25 +0100

    scripts work fine

--- LaunchMulitplayer.bat
+++ LaunchMulitplayer.bat
@@ -4,22 +4,22 @@
 :: after the turn it should upload the last savefile to github
 
 :: fetch newest save from github
+@echo off
 set /p turn=< turncount.txt
 echo %turn%
 git pull origin tm
 set /p turn_remote=< turncount.txt
 echo %turn_remote%
-pause
-::if %turn_remote% gtr %turn% (
-copy save.GM2 ..\games\yourturn.GM2
-echo you have a new save, start HOMM and play your turn
-::launch HOMMIII
-..\HEROES3HD.exe
-::) else(
-echo there is no new save
-::)
-pause
-echo next stage will upload save
-pause
-call UploadSave.bat
+IF %turn_remote% GTR %turn% (
+    copy save.GM2 ..\games\yourturn.GM2
+	echo you have a new save, start HOMM and play your turn
+	pause
+	::launch HOMMIII
+	call startHeroes.bat
+	echo next stage will upload save
+	pause
+	call UploadSave.bat
+) ELSE (
+	echo there is no new save
+)
 pause
--- UploadSave.bat
+++ UploadSave.bat
@@ -4,14 +4,11 @@
 
 :: upload latest autosave to github
 copy "..\games\AUTOSAVE.GM2" "save.GM2"
-pause
 set /p turn=< turncount.txt
-pause
 set /a next_turn=((%turn% + 1))
 echo %next_turn%
-pause
 echo %next_turn% > turncount.txt
 git add .   
 git commit -m "%next_turn%"
 git push origin tm
- 
+pause
